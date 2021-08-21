fe23672bd3aed799895aacde3ba96f0f3183135d
Karen Carnaval <karencarnaval4@gmail.com>
2021-08-20 23:13:55 -0400

    header

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,16 @@ const Layout = ({ location, title, children }) => {
 
   if (isRootPath) {
     header = (
+      <>
       <h1 className="main-heading">
         <Link to="/">{title}</Link>
       </h1>
+      <nav>
+        <h5>
+          <Link to="/about">about</Link>
+        </h5>
+      </nav>
+      </>
     )
   } else {
     header = (
